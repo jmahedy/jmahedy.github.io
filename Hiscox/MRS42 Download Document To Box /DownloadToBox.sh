44b68aed7470b88c6183be745676ac2a051d4ef9
jmahedy <jmahedy@box.com>
2019-06-20 15:25:07 +0100

    s
    s

---
+++ Hiscox/MRS42 Download Document To Box /DownloadToBox.sh
@@ -0,0 +1,8 @@
+
+/bin/bash
+
+token=n0nCX4VEBXaAYr6apd5Bq4m8n8whLMb3
+
+for file in  /⁨Users⁩/jmahedy/Documents⁩/Development/jmahedy.github.io⁩/Hiscox⁩/⁨MRS42/documents/*; do
+    echo "$(basename "$file")"
+done
---
+++ Hiscox/MRS42/DownloadToBox.sh
@@ -0,0 +1,3 @@
+for d in /Users/jmahedy/Documents/Development/jmahedy.github.io/Hiscox/MRS42/documents/* ; do
+   echo "$d"
+done
